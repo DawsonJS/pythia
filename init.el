070b202b821dc8543523a30d55a13491ef40a90e
Dawson Schraiber <dschraiber1@gmail.com>
2023-01-18 15:20:29 -0500

    evil mode & tabs to spaces

--- init.el
+++ init.el
@@ -3,6 +3,11 @@
              '("melpa" . "http://melpa.org/packages/") t)
 
 (require 'org)
+(require 'goto-chg)
+(require 'undo-fu)
+(require 'evil)
+(setq-default indent-tabs-mode nil)
+(evil-mode 1)
 (setq inhibit-startup-message t  ; Don't show the splash screen
       visible-bell t)            ; Flash when the bell rings
 
@@ -11,7 +16,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(org-roam org)))
+ '(package-selected-packages '(goto-chg undo-fu evil org-roam org)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
