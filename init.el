d848ff85fb225d2bd29b0111ba65e190ae4eddd6
Dawson Schraiber <dschraiber@vt.edu>
2023-01-17 20:07:16 -0500

    first commit

---
+++ init.el
@@ -0,0 +1,16 @@
+(require 'org)
+(setq inhibit-startup-message t  ; Don't show the splash screen
+      visible-bell t)            ; Flash when the bell rings
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(org)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
