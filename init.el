1d8d8350f0a4b77f149b5263ec299d0b30333c43
Dawson Schraiber <dschraiber1@gmail.com>
2023-01-19 00:18:47 -0500

    problem with packages on macos

--- init.el
+++ init.el
@@ -2,6 +2,8 @@
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 
+(add-to-list 'load-path "~/.emacs.d")
+
 (require 'org)
 (require 'goto-chg)
 (require 'undo-fu)
@@ -10,16 +12,3 @@
 (evil-mode 1)
 (setq inhibit-startup-message t  ; Don't show the splash screen
       visible-bell t)            ; Flash when the bell rings
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(goto-chg undo-fu evil org-roam org)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
