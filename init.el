8713380551ac4e700d1d5ed10fe38c71f855dd51
Dawson Schraiber <dschraiber1@gmail.com>
2023-01-19 10:38:15 -0500

    evil mode search & separate custom.el file gen

--- init.el
+++ init.el
@@ -29,19 +29,11 @@
 (require 'goto-chg)
 (require 'undo-fu)
 (require 'evil)
+(evil-select-search-module 'evil-search-module 'evil-search)
 (setq-default indent-tabs-mode nil)
 (evil-mode 1)
 (setq inhibit-startup-message t  ; Don't show the splash screen
       visible-bell t)            ; Flash when the bell rings
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil undo-fu org org-roam goto-chg)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
